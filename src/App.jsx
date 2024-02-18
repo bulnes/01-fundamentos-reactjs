924f297b0878a1171791a80e1b9bdc7ae77b625a
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-02-18 04:48:28 +0000

    feat: add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post author="John" content="Hello, world!" />
       <Post author="Jane" content="React is great!" />
     </div>
