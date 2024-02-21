d2b7f0abbc774e04ee0363486ed0372bea775dbc
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-02-21 01:49:25 +0000

    feat: create comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/bulnes.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Bruno Bulnes</strong>
+              <time
+                title="10 de novembro de 2021 às 08:13"
+                dateTime="2021-09-10 08:13:00"
+              >
+                Cerce de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+            <button>
+                <ThumbsUp size={20} />
+                Aplaudir <span>03</span>
+            </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
