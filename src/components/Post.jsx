702adc7521d05bf1fd7ea5204018c1d8a5e158d3
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-02-29 22:41:04 -0300

    feat: use state for comments list

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,13 @@
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([]);
+
   const publishedDateTitle = format(publishedAt, "dd 'de' MMMM 'às' HH:mm'h'", {
     locale: ptBR,
   });
@@ -13,6 +16,12 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   });
+
+  function handleCreationNewComment(e) {
+    e.preventDefault();
+
+    setComments([...comments, comments.length + 1]);
+  }
 
   return (
     <article className={styles.post}>
@@ -53,7 +62,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreationNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback:</strong>
 
         <textarea placeholder="Deixe um comentário" />
@@ -64,9 +73,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
